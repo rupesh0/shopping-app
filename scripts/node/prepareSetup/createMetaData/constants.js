a4d976fc274be39dcd41b15cd957f881c3e5fffa
Rupesh Prajapat <rp15498@gmail.com>
2025-09-10 21:53:10 +0530

    Added metaData Types & Defination

---
+++ scripts/node/prepareSetup/createMetaData/constants.js
@@ -0,0 +1,34 @@
+const META_DATA_TYPES = {
+  APEX_CLASS: "ApexClass",
+  APEX_TRIGGER: "ApexTrigger",
+  LIGHTNING_WEB_COMPONENT: "LightningComponentBundle"
+};
+
+const META_DATA_DEFINATIONS = {
+  [META_DATA_TYPES.APEX_CLASS]: {
+    label: "Apex Class",
+    directory: "classes",
+    suffix: ".cls",
+    metaSuffix: ".cls-meta.xml",
+    alwaysCreateMeta: true
+  },
+  [META_DATA_TYPES.APEX_TRIGGER]: {
+    label: "Apex Trigger",
+    directory: "triggers",
+    suffix: ".trigger",
+    metaSuffix: ".trigger-meta.xml",
+    alwaysCreateMeta: true
+  },
+  [META_DATA_TYPES.LIGHTNING_WEB_COMPONENT]: {
+    label: "Lightning Web Component",
+    directory: "lwc",
+    suffix: "  .js",
+    metaSuffix: ".js-meta.xml",
+    alwaysCreateMeta: false
+  }
+};
+
+module.exports = {
+  META_DATA_TYPES,
+  META_DATA_DEFINATIONS
+};
